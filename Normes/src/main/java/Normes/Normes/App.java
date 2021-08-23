7b5c4d080ba140c8ba8d542c651aa8aba0d3afd4
pranavdoshi2907 <pranavdoshi2907@gmail.com>
2021-08-23 22:50:38 +0530

    first

---
+++ Normes/src/main/java/Normes/Normes/App.java
@@ -0,0 +1,13 @@
+package Normes.Normes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
